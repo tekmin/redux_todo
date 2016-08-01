b298219a63a92b6524bb8b6d897da4d6282f404e
tekmin <tekmin.ewe@seekasia.com>
2016-08-01 17:42:53 +0800

    Create todo form

---
+++ src/components/Todo.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import {
+    ListView,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableHighlight,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+
+const dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+const styles = StyleSheet.create({
+    container: {
+        marginHorizontal: 10,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: 'gray',
+        height: 40,
+        marginTop: 40,
+        padding: 10,
+    },
+    button: {
+        textAlign: 'center',
+        backgroundColor: 'black',
+        color: 'white',
+        marginVertical: 10,
+        padding: 10,
+    },
+});
+
+class Todo extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onTextChange = this.onTextChange.bind(this);
+        this.save = this.save.bind(this);
+        this.renderRow = this.renderRow.bind(this);
+        
+        this.state = {
+            text: '',
+        };
+    }
+
+    onTextChange(text) {
+        this.setState({
+            text: text,
+        });
+    }
+
+    save() {
+    }
+
+    renderRow(rowData) {
+        return (
+            <TouchableHighlight onPress={() => {}}>
+                <Text>{rowData.text}</Text>
+            </TouchableHighlight>
+        );
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={this.onTextChange}
+                    value={this.state.text}
+                />
+                <TouchableOpacity onPress={this.save}>
+                    <Text style={styles.button}>Add ToDo</Text>
+                </TouchableOpacity>
+                <Text>To Do List</Text>
+                <ListView 
+                    dataSource={dataSource.cloneWithRows([])}
+                    renderRow={this.renderRow}
+                    enableEmptySections={true}
+                />
+            </View>
+        )
+    }
+}
+
+module.exports = Todo;
