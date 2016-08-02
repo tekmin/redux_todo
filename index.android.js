39c5bdf48a007aa1bb8b3c553d5a01a14885248d
tekmin <tekmin.ewe@seekasia.com>
2016-08-02 14:46:08 +0800

    5.3 - Repeat setup for android

--- index.android.js
+++ index.android.js
@@ -8,12 +8,19 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
 } from 'react-native';
-import ToDo from './src/components/ToDo';
+import Todo from './src/components/Todo';
+import { createStore } from 'redux';
+import RootReducers from './src/reducers/RootReducers';
+import { Provider } from 'react-redux';
+
+let store = createStore(RootReducers);
 
 class ReduxTodo extends Component {
   render() {
     return (
+      <Provider store={store}>
         <Todo />
+      </Provider>
     );
   }
 }
